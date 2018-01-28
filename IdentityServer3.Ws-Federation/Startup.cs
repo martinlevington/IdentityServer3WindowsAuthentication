efe9e48958d830174154d1a7aa053f4fa974b36a
martin levington <martin.levington@gmail.com>
2018-01-28 23:32:24 +0000

    init commit

---
+++ IdentityServer3.Ws-Federation/Startup.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using IdentityServer3.Core.Configuration;
+using IdentityServer3.Core.Resources;
+using IdentityServer3.Core.Services;
+using IdentityServer3.Ws_Federation.Configuration;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.WsFederation;
+using Owin;
+using Serilog;
+using Scopes = IdentityServer3.Ws_Federation.Configuration.Scopes;
+
+[assembly: OwinStartup(typeof(IdentityServer3.Ws_Federation.Startup))]
+
+
+namespace IdentityServer3.Ws_Federation
+{
+    public class Startup
+    {
+
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Trace(outputTemplate: "{Timestamp} [{Level}] ({Name}){NewLine} {Message}{NewLine}{Exception}")
+                .CreateLogger();
+
+            var factory = new IdentityServerServiceFactory()
+                .UseInMemoryClients(Clients.Get())
+                .UseInMemoryScopes(Scopes.Get());
+            factory.UserService = new Registration<IUserService>(typeof(ExternalRegistrationUserService));
+
+            var options = new IdentityServerOptions
+            {
+                SigningCertificate = Certificate.Load(),
+                Factory = factory,
+                AuthenticationOptions = new AuthenticationOptions
+                {
+                    EnableLocalLogin = false,
+                    IdentityProviders = ConfigureIdentityProviders
+                }
+            };
+
+            appBuilder.UseIdentityServer(options);
+        }
+
+        private void ConfigureIdentityProviders(IAppBuilder app, string signInAsType)
+        {
+            var wsFederation = new WsFederationAuthenticationOptions
+            {
+                AuthenticationType = "windows",
+                Caption = "Windows",
+                SignInAsAuthenticationType = signInAsType,
+
+                MetadataAddress = "https://localhost:44366",
+                Wtrealm = "urn:idsrv3"
+            };
+            app.UseWsFederationAuthentication(wsFederation);
+        }
+
+    }
+}
---
+++ IdentityServer3WindowsAuthentication/Startup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using IdentityServer.WindowsAuthentication.Configuration;
+using IdentityServer3WindowsAuthentication.Configuration;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(IdentityServer3WindowsAuthentication.Startup))]
+
+namespace IdentityServer3WindowsAuthentication
+{
+    public class Startup
+    {
+
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseWindowsAuthenticationService(new WindowsAuthenticationOptions
+            {
+                IdpReplyUrl = "https://localhost:44377/was",
+                SigningCertificate = Certificate.Load(),
+                EnableOAuth2Endpoint = false
+            });
+        }
+    }
+}
